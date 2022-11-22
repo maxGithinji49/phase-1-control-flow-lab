a09e461dafdb33d3cf23c84b2108a84b59f559a7
Max <maxwell.githinji@student.moringaschool.com>
2022-11-22 21:20:05 +0300

    Done

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(Distance){
   // Write your code here!
+  if (Distance <= 400) {
+    return 'This one is on me!'
+  }
+  else if (Distance > 2000 && Distance < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (Distance > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  }
+  else if (city !== 'NYC') {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === 'generous') {
+    return 'Thank you so much.'
+  }
+  else if (tip === 'not as generous') {
+    return 'Thank you.'
+  }
+  else if (tip = 'anything else') {
+    return 'Bye.'
+  }
 }
